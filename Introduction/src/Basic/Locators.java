60801c7e018b308721814e61e896900f0e239ad3
Surya <suryamadhavan6383@gmail.com>
2025-07-07 10:18:47 +0530

    login Commit Successfull

--- Introduction/src/Basic/Locators.java
+++ Introduction/src/Basic/Locators.java
@@ -1,13 +1,17 @@
 package Basic;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
+@SuppressWarnings("deprecation")
 public class Locators {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.manage().window().maximize();
 		driver.findElement(By.id("inputUsername")).sendKeys("Surya");
@@ -25,6 +29,28 @@ public class Locators {
 		else {
 			System.out.println("Login failed, Test Case fail, System throws-"+AR);
 		}
+		String password=getpassword(driver);
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector(".go-to-login-btn")).submit();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("input#inputUsername")).sendKeys("Surya");
+		Thread.sleep(2000);
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@value='rmbrUsername']")).click();
+		driver.findElement(By.cssSelector("input[value='agreeTerms']")).click();
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).submit();
+		Thread.sleep(5000);
+		String Result = driver.findElement(By.tagName("p")).getText();
+		System.out.println(Result);
+		Assert.assertEquals("You are successfully logged in.", Result);
+		driver.findElement(By.className("logout-btn")).click();
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+	public static String getpassword(WebDriver driver) throws InterruptedException
+	{
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.linkText("Forgot your password?")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//*[@placeholder=\"Name\"]")).sendKeys("Surya");
@@ -34,18 +60,9 @@ public class Locators {
 		driver.findElement(By.cssSelector("input[type='text']:nth-child(4)")).sendKeys("9876543210");
 		Thread.sleep(3000);
 		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
-		Thread.sleep(3000);
-		String Pass =driver.findElement(By.className("infoMsg")).getText();
-		Thread.sleep(2000);
-		driver.findElement(By.cssSelector(".go-to-login-btn")).submit();
-		Thread.sleep(3000);
-		driver.findElement(By.cssSelector("input#inputUsername")).sendKeys("Surya");
-		driver.findElement(By.xpath("input[placeholder*='Pass']")).sendKeys("raghulshetty");
-		driver.findElement(By.xpath("//input[@value='rmbrUsername']")).click();
-		driver.findElement(By.cssSelector("input[value='agreeTerms']")).click();
-		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).submit();
-		driver.quit();
+		String pass=driver.findElement(By.cssSelector(".infoMsg")).getText();
+		String[] split_password=pass.split("'");
+		String Password=split_password[1].split("'")[0];
+		return Password;
 	}
-
 }
-;
